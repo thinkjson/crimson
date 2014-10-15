7ef3753d09a0a99769f82d63701e1bf1d6f91c86
Mark Cahill <mark@thinkjson.com>
2014-10-14 22:19:13 -0400

    Fixed bogus icon

--- platforms/android/ant-build/HelloWorld.ap_.d
+++ platforms/android/ant-build/HelloWorld.ap_.d
@@ -3,7 +3,6 @@
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-land-ldpi/screen.png \
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-mdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-mdpi/screen.png \
-/home/thinkjson/Code/crimson/platforms/android/res/drawable-hdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-hdpi/screen.png \
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-xhdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/res/drawable-xhdpi/screen.png \
@@ -18,7 +17,6 @@
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-mdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-mdpi/screen.png \
-/home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-hdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-hdpi/screen.png \
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /home/thinkjson/Code/crimson/platforms/android/ant-build/res/drawable-xhdpi/screen.png \
